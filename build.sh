6b54db6e7c0d395e230758c772141487c2c4ec95
woody <woody@gate.me>
2024-08-09 16:15:19 +0800

    1

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -o webhook-linux webhook.go
+# GOOS=linux GOARCH=amd64 go build -o webhook-linux webhook.go
+GOOS=linux GOARCH=amd64 go build -o mongoclient mongo.go
---
+++ mongo.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	uri := os.Args[1]
+	if uri == "" {
+		panic("uri can not be blank.")
+	}
+	fmt.Println("mongo uri: ", uri)
+	// 创建一个新的客户端并连接到 MongoDB
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 设置一个 10 秒的连接超时上下文
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// 连接到 MongoDB
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 确保在程序退出时断开连接
+	defer client.Disconnect(ctx)
+	fmt.Println("mongodb connected.")
+}
