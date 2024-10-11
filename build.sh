dcd80b79ce17f5b3b33cdbe3f0012c81f4fd1e38
woody <woody@gate.me>
2024-10-11 17:19:42 +0800

    12321

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # GOOS=linux GOARCH=amd64 go build -o webhook-linux webhook.go
-GOOS=linux GOARCH=amd64 go build -o mongoclient mongo.go
+# GOOS=linux GOARCH=amd64 go build -o mongoclient mongo.go
+GOOS=linux GOARCH=amd64 go build  -o tcp12321 tcp12321.go
