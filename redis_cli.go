ba12bc173dc8fd2deca2f13d04b0a3747fb53bf8
woody <woody@gate.me>
2024-11-06 17:30:01 +0800

    2332

--- redis_cli.go
+++ redis_cli.go
@@ -34,4 +34,26 @@ func main() {
 	fmt.Println("Connected to Redis")
 	// 关闭 Redis 客户端
 	defer rdb.Close()
+
+	// 查看所有的键
+	fmt.Println("All keys in Redis:")
+	var cursor uint64
+	for {
+		// 使用 SCAN 命令查找所有的键
+		keys, nextCursor, err := rdb.Scan(ctx, cursor, "*", 0).Result()
+		if err != nil {
+			log.Fatalf("Could not scan keys: %v", err)
+		}
+
+		// 打印当前批次的键
+		for _, key := range keys {
+			fmt.Println(key)
+		}
+
+		// 如果 nextCursor 为 0，表示没有更多的键
+		if nextCursor == 0 {
+			break
+		}
+		cursor = nextCursor
+	}
 }
